af17d58b0f70626434f04b1587b3dc32b373982d
Kacper <68641248+neomat-prog@users.noreply.github.com>
2023-05-04 11:25:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+toggle = document.querySelector(".toggle");
+
+let getMode = localStorage.getItem("mode");
+if(getMode && getMode === "dark") {
+    body.classList.add("dark");
+    toggle.classList.add("active");
+}
+console.log(getMode);
+
+toggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if(!body.classList.contains("dark")){
+        return localStorage.setItem("mode", "light");
+    }
+        localStorage.setItem("mode", "dark");
+});
+
+toggle.addEventListener('click', () => toggle.classList.toggle("active"));
